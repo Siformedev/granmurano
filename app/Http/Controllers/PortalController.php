37c7d9f871bbe05022e0dddbadb1bd55a9d8b056
Roberto <roberto.ht2ml@gmail.com>
2021-01-31 18:40:17 -0300

    teste

--- app/Http/Controllers/PortalController.php
+++ app/Http/Controllers/PortalController.php
@@ -1905,9 +1905,9 @@ $parcelsModel->delete();
 
  
        
-        $notificationCode = "00C22320-AA64-4B82-B803-98C546D5683F";
+        $notificationCode = "00C22320-AA64-4B82-B803-98C546D5683F"; 
         $contrato_id=3;
-        $transaction = $pseg->consultarTransacao($notificationCode,$contrato_id);
+        $transaction = $pseg->consultarTransacao('430AC13B-819D-4501-A981-B1E489DCBA65',$contrato_id);
         dd($transaction);
     }
 
