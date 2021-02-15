412e0aa83df11aa674ee4fa27941f2bae7630697
Roberto <roberto.ht2ml@gmail.com>
2021-02-15 14:29:05 -0300

    teste

--- resources/views/gerencial/formandos/show_item.blade.php
+++ resources/views/gerencial/formandos/show_item.blade.php
@@ -172,6 +172,7 @@
                                     <tr>
                                         <td class="text-center">{{$parcela['id']}}</td>
                                         <td class="text-center">{{App\ParcelasPagamentos::where('parcela_id',$parcela['id'])->get()}}</td>
+                                        <td class="text-center">{{App\PagamentosBoleto::where('parcela_pagamentos',$parcela['id'])->get()}}</td>
                                         <td class="text-center">{{date('d/m/Y', strtotime($parcela['dt_vencimento']))}}</td>
                                         <td class="text-center">{{number_format($parcela['valor'],2, ",", ".")}}</td>
                                         <td class="text-center"> {!! $actionParc !!} </td>
